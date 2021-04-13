1c12d6c753b9cb5fe9af9c75ef31899946131c88
vasfer <vasferraro@gmail.com>
2021-04-13 17:44:32 +0100

    gh-pages and refactor

--- src/API/index.ts
+++ src/API/index.ts
@@ -15,12 +15,25 @@ export async function request(method: string, url: string, params = "") {
 export async function getDaysGames(date: string): Promise<Game[]> {
     const opt = await request("GET", "http://data.nba.net/10s/prod/v1/today.json")
     const season = opt.seasonScheduleYear 
-    console.log(opt)
+    // console.log(opt)
     const teams = await request("GET", `http://data.nba.net/10s/prod/v2/${season}/teams.json`)
+
+    // console.log(teams)
     const daysGames = await request("GET", `http://data.nba.net/10s/prod/v1/${date}/scoreboard.json`)
     // console.log(daysGames)
     let todaysGames: Game[]=[]
 
+    const players = await request("GET", `http://data.nba.net/10s/prod/v1/2020/players.json`)
+    let playerId = ""
+    players.league.standard.forEach((player: any) => {
+        if (player.firstName=="Stephen"){
+            playerId=player.personId
+        }
+    })
+    console.log(playerId)
+    const player = await request("GET", `http://data.nba.net/10s/prod/v1/2020/players/${playerId}_profile.json`)
+
+    console.log(player)
     daysGames.games.forEach((game: any) => {
         const hTeamFullname = teams.league.standard.filter((team: any) => team.teamId === game.hTeam.teamId)[0].fullName
         const vTeamFullname = teams.league.standard.filter((team: any) => team.teamId === game.vTeam.teamId)[0].fullName
